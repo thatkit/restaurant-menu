9876c07efb0b4ea49587ff9421b334770165e6fd
Nikita <emper137137@gmail.com>
2022-05-14 09:31:02 +1000

    initial boilerplate

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}...`));
