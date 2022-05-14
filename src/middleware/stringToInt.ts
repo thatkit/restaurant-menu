44c62e8c183c5bb1ab3e75dbf2ad93578a352d07
Nikita <emper137137@gmail.com>
2022-05-14 19:32:30 +1000

    all fields formdata request is set

--- src/middleware/stringToInt.ts
+++ src/middleware/stringToInt.ts
@@ -1,6 +1,20 @@
 import { NextFunction, Request, Response } from "express";
 
-export const stringToInt = (req: Request, res: Response, next: NextFunction) => {
+export const stringToInt = (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
 	req.body.price = +req.body.price;
+	let energy = req.body.energy;
+	if (energy) {
+		energy = {
+			valuePer100g: +energy.valuePer100g,
+			prots: +energy.prots,
+			carbs: +energy.carbs,
+			fats: +energy.fats,
+		};
+		req.body.energy = energy;
+	}
 	next();
 };
--- src/prisma/queries.ts
+++ src/prisma/queries.ts
@@ -1,8 +1,16 @@
 import { PrismaClient, Prisma } from "@prisma/client";
 const prisma = new PrismaClient();
 
-export const CREATE_NEW_COURSE = async (data: Prisma.CourseCreateInput) => {
-	return await prisma.course.create({ data });
+export const CREATE_NEW_COURSE = async (data: any) => {
+	const { energy, ...mainData } = data;
+	return await prisma.course.create({
+		data: {
+			...mainData,
+			energy: {
+				create: energy,
+			},
+		},
+	});
 };
 
 export const CREATE_NEW_CUISINE = async (data: Prisma.CuisineCreateInput) => {
